e10528fe2854aee583074775915ad6f53e009766
Babis Karypidis <xabikos@hotmail.com>
2015-03-10 19:59:58 +0200

    Remove full asp.net mvc project and keep only the required files

--- SkgAzureBootcamp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SkgAzureBootcamp.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- SkgAzureBootcamp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SkgAzureBootcamp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
